3a454f95dfb046043c53e92ba8dd7cc2496a1600
adam <adamsiedlec@wp.pl>
2021-04-22 21:12:16 +0200

    i have done some changes to connection strategy

--- src/main/java/pl/adamsiedlecki/OTM/dataFetcher/DataFetcher.java
+++ src/main/java/pl/adamsiedlecki/OTM/dataFetcher/DataFetcher.java
@@ -7,6 +7,9 @@ import pl.adamsiedlecki.OTM.db.tempData.TemperatureData;
 import pl.adamsiedlecki.OTM.db.tempData.TemperatureDataService;
 import pl.adamsiedlecki.OTM.tools.HtmlToTemperatureData;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.time.LocalDateTime;
@@ -27,29 +30,72 @@ public class DataFetcher {
         this.temperatureDataService = temperatureDataService;
     }
 
-    public List<TemperatureData> fetch(){
+    public List<TemperatureData> fetch() {
         String content = getHtml();
 
         List<TemperatureData> temperatureData = htmlToData.process(content);
+
+        // in case of blank response
+        if (temperatureData.size() == 0) {
+            System.out.println("There are no temperatures fetched!!!");
+            sendRestartCommand();
+            try {
+                Thread.sleep(2500);
+                content = getHtml();
+                temperatureData = htmlToData.process(content);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
         LocalDateTime now = LocalDateTime.now();
-        for(TemperatureData td: temperatureData){
+        for (TemperatureData td : temperatureData) {
             td.setDate(now);
         }
         temperatureDataService.saveAll(temperatureData);
+
         return temperatureData;
     }
 
     private String getHtml() {
         String content = null;
         URLConnection connection = null;
+        int status = 0;
         try {
-            connection =  new URL(apiAddress).openConnection();
+            URL url = new URL(apiAddress);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setReadTimeout(25000);
+            status = con.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                stringBuilder.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            content = stringBuilder.toString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(status + "HTML content: " + content);
+        return content;
+    }
+
+    private String sendRestartCommand() {
+        String content = null;
+        URLConnection connection = null;
+        try {
+            connection = new URL(apiAddress + "/restart").openConnection();
             connection.setReadTimeout(25000);
             Scanner scanner = new Scanner(connection.getInputStream());
             scanner.useDelimiter("\\Z");
             content = scanner.next();
             scanner.close();
-        }catch ( Exception ex ) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return content;
