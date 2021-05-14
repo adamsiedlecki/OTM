e0d7ea8c38d2b14469431b56e9d28d8f4a07e156
adam <adamsiedlec@wp.pl>
2021-05-14 13:57:45 +0200

    i have changed optional to list in repo

--- src/main/java/pl/adamsiedlecki/OTM/db/location/LocationRepo.java
+++ src/main/java/pl/adamsiedlecki/OTM/db/location/LocationRepo.java
@@ -4,11 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface LocationRepo extends JpaRepository<Location, Long> {
 
     @Query("SELECT l FROM Location l WHERE l.latitude=?1 AND l.longitude=?2")
-    Optional<Location> findByLatitudeAndLongitude(float latitude, float longitude);
+    List<Location> findByLatitudeAndLongitude(float latitude, float longitude);
 }
