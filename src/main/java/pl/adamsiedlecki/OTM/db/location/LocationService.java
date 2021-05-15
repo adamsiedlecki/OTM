9a3ecfe30cb1285b4286cfa2dcd3c77f04832e4a
adam <adamsiedlec@wp.pl>
2021-05-15 16:40:16 +0200

    findAll

--- src/main/java/pl/adamsiedlecki/OTM/db/location/LocationService.java
+++ src/main/java/pl/adamsiedlecki/OTM/db/location/LocationService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LocationService {
@@ -27,15 +28,17 @@ public class LocationService {
     }
 
     public Location getOrSave(String latitude, String longitude) {
-        Optional<Location> opLoc = getLocationByCords(latitude, longitude);
-//        List<Location> all = locationRepo.findAll();
-//        List<Location> list = all.stream().filter(e -> e.getLatitude().equals(latitude) && e.getLongitude().equals(longitude)).collect(Collectors.toList());
-//        Optional<Location> opLoc;
-//        if (list.isEmpty()) {
-//            opLoc = Optional.empty();
-//        } else {
-//            opLoc = Optional.of(list.get(0));
-//        }
+        //Optional<Location> opLoc = getLocationByCords(latitude, longitude);
+        List<Location> all = locationRepo.findAll();
+        System.out.println("ALL: " + all);
+        System.out.println("SEARCH FOR : " + latitude + "  " + longitude);
+        List<Location> list = all.stream().filter(e -> e.getLatitude().equals(latitude) && e.getLongitude().equals(longitude)).collect(Collectors.toList());
+        Optional<Location> opLoc;
+        if (list.isEmpty()) {
+            opLoc = Optional.empty();
+        } else {
+            opLoc = Optional.of(list.get(0));
+        }
 
         if (opLoc.isPresent()) {
             System.out.println("GOT LOCATION BY lat and lng: " + opLoc.get());
