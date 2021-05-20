4f0124a0a85ce4e240079cff92730d5d697413c5
adam <adamsiedlec@wp.pl>
2021-05-20 11:43:43 +0200

    delete unnecessary commented code

--- src/main/java/pl/adamsiedlecki/OTM/controller/notSecured/LoginController.java
+++ src/main/java/pl/adamsiedlecki/OTM/controller/notSecured/LoginController.java
@@ -14,13 +14,6 @@ public class LoginController {
         return "login";
     }
 
-//    @PostMapping("/login")
-//    public String postLogin(@ModelAttribute User user, Model model) {
-//        System.out.println("USER: "+user);
-//        System.out.println(user.getUsername());
-//        return "mainPage";
-//    }
-
     @GetMapping("/login-error")
     public String getLoginWithError(Model model) {
         model.addAttribute("loginError", true);
